043ec14543e6b5e8b1ebab024fc9c0005482a2da
Alphonsus Jovian Joy Reynaldo <55451337+jovianjr@users.noreply.github.com>
2022-11-06 00:39:51 +0700

    Feature/basic components elements (#2)
    * feat(styles): add font poppins
    
    * feat(jsconfig): set intellisense
    
    * feat(package): add library
    
    * feat(elements): add button
    
    * feat(elements): add text field

---
+++ src/views/elements/button/index.jsx
@@ -0,0 +1,56 @@
+import clsx from 'clsx';
+import { useMemo } from 'react';
+
+/**
+ * @typedef ButtonProps
+ * @property {React.ReactNode} children
+ * @property {string} [className]
+ * @property {('primary'|'secondary')} variant
+ * @property {JSX.Element} [startIcon]
+ * @property {JSX.Element} [endIcon]
+ * @property {string} [href]
+ * @property {boolean} [disabled]
+ * @property {React.MouseEvent} [onClick]
+ */
+
+/**
+ * @param {ButtonProps} props
+ */
+const Button = ({
+	children = null,
+	className = '',
+	variant = 'primary',
+	startIcon = null,
+	endIcon = null,
+	href = null,
+	disabled = false,
+	onClick = () => {},
+}) => {
+	const StartIcon = startIcon;
+	const EndIcon = endIcon;
+
+	const Element = useMemo(() => (href ? 'a' : 'button'), [href]);
+
+	return (
+		<Element
+			className={clsx(
+				'rounded border border-transparent py-2 px-3 text-sm font-medium transition-all',
+				variant === 'primary' &&
+					'bg-purple-700 text-white hover:border-purple-700 hover:bg-white hover:text-purple-700',
+				variant === 'secondary' &&
+					'bg-slate-200 text-purple-700 hover:border-purple-700 hover:bg-purple-700 hover:text-white',
+				'disabled:bg-gray-300 disabled:hover:border-transparent disabled:hover:text-white',
+				className
+			)}
+			disabled={disabled}
+			onClick={onClick}
+			href={href}
+		>
+			{startIcon ? <StartIcon className="mr-2 w-4" /> : null}
+			{children}
+			{endIcon ? <EndIcon className="ml-2 w-4" /> : null}
+		</Element>
+	);
+};
+
+export default Button;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@
 module.exports = {
 	content: ['./src/**/*.{js,ts,jsx,tsx}'],
 	theme: {
+		fontFamily: {
+			poppin: ['Poppins'],
+		},
 		extend: {},
 	},
 	plugins: [],
