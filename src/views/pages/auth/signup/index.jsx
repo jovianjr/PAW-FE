7d05c6ec4de538f9074905bd8f0d1baa5d342e7a
jovian <alphonsusjovian@gmail.com>
2022-11-12 00:38:51 +0700

    feat(auth): add signup page

--- src/views/pages/auth/signup/index.jsx
+++ src/views/pages/auth/signup/index.jsx
@@ -1,5 +1,78 @@
+import { useForm } from 'react-hook-form';
+
+import Button from '@/views/elements/button';
+import TextField from '@/views/elements/text-field';
+
+import AuthLayout from '@/views/layouts/auth-layout';
+
 const SignUp = () => {
-	return <>SignUp</>;
+    const { control, handleSubmit } = useForm({
+        defaultValues: {
+            email: '',
+            username: '',
+            password: '',
+            confirm_password: ''
+        }
+    });
+
+    const onSubmit = () => {
+        alert('submitted');
+    };
+
+    return (
+        <AuthLayout onSubmit={handleSubmit(onSubmit)}>
+            <h1 className="w-full text-center text-2xl font-semibold">
+                CREATE ACCOUNT
+            </h1>
+            <div className="flex w-full flex-col gap-4">
+                <TextField
+                    control={control}
+                    className="w-full"
+                    name="email"
+                    fieldName="email"
+                    placeholder="email"
+                    type="email"
+                    rules={{ required: true }}
+                />
+                <TextField
+                    control={control}
+                    className="w-full"
+                    name="username"
+                    fieldName="Username"
+                    placeholder="Username"
+                    rules={{ required: true }}
+                />
+                <TextField
+                    control={control}
+                    className="w-full"
+                    name="password"
+                    fieldName="password"
+                    placeholder="Password"
+                    type="password"
+                    rules={{ required: true }}
+                />
+                <TextField
+                    control={control}
+                    className="w-full"
+                    name="confirm_password"
+                    fieldName="Confirm Password"
+                    placeholder="Confirm Password"
+                    type="password"
+                    rules={{ required: true }}
+                />
+            </div>
+
+            <div className="flex w-full items-center justify-between">
+                <p className="text-sm">
+                    Already have an account?{' '}
+                    <a href="/login" className="font-semibold text-purple-700">
+                        Login
+                    </a>
+                </p>
+                <Button type="submit">Sign Up</Button>
+            </div>
+        </AuthLayout>
+    );
 };
 
 export default SignUp;
