c0adc557fad0faf274e87197d413c7d3ace1c67d
jovian <alphonsusjovian@gmail.com>
2022-11-12 00:38:43 +0700

    feat(auth): add login page

--- src/views/pages/auth/login/index.jsx
+++ src/views/pages/auth/login/index.jsx
@@ -1,5 +1,66 @@
+import { useForm } from 'react-hook-form';
+
+import Button from '@/views/elements/button';
+import TextField from '@/views/elements/text-field';
+
+import AuthLayout from '@/views/layouts/auth-layout';
+
 const Login = () => {
-	return <>Login</>;
+    const { control, handleSubmit } = useForm({
+        defaultValues: {
+            username: '',
+            password: ''
+        }
+    });
+
+    const onSubmit = () => {
+        alert('submitted');
+    };
+
+    return (
+        <AuthLayout onSubmit={handleSubmit(onSubmit)}>
+            <h1 className="w-full text-center text-2xl font-semibold">
+                Reset your password
+            </h1>
+            <div className="flex w-full flex-col gap-4">
+                <TextField
+                    control={control}
+                    className="w-full"
+                    name="username"
+                    fieldName="Username"
+                    placeholder="Username"
+                    rules={{ required: true }}
+                />
+                <div className="flex w-full flex-col items-end gap-2">
+                    <TextField
+                        control={control}
+                        className="w-full"
+                        name="password"
+                        fieldName="Password"
+                        placeholder="Password"
+                        type="password"
+                        rules={{ required: true }}
+                    />
+                    <a
+                        href="/forgot-password"
+                        className="text-sm text-purple-700"
+                    >
+                        Forgot password?
+                    </a>
+                </div>
+            </div>
+
+            <div className="flex w-full items-center justify-between">
+                <p className="text-sm">
+                    Don’t have any account?{' '}
+                    <a href="/signup" className="font-semibold text-purple-700">
+                        Sign Up
+                    </a>
+                </p>
+                <Button type="submit">Login</Button>
+            </div>
+        </AuthLayout>
+    );
 };
 
 export default Login;
