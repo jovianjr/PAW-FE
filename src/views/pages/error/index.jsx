d819effff2b34fe4115960566fb6e4210c39cf4b
aulianurfajriyah <79237668+aulianurfajriyah@users.noreply.github.com>
2022-11-12 19:39:32 +0700

    feat(error-page): add main layout to 404 page (#13)

--- src/views/pages/error/index.jsx
+++ src/views/pages/error/index.jsx
@@ -1,16 +1,27 @@
 import { useRouteError } from 'react-router-dom';
 import StatusResult from '@/views/components/status-result';
+import MainLayout from '@/views/layouts/main-layout';
 
 const ErrorBoundary = () => {
-	let error = useRouteError();
-	if (error.status === 404) {
-		return (
-			<StatusResult type="warning" title="404" detail="Page not found" titleClassName='text-[64px]' detailClassName='text-[24px]'/>
-		)
-	}
-	return (
-		<StatusResult title="Sorry, an unexpected error has occurred" detail={error.statusText || error.message}/>
-	);
+    let error = useRouteError();
+    return (
+        <MainLayout>
+            {error.status === 404 ? (
+                <StatusResult
+                    type="warning"
+                    title="404"
+                    detail="Page not found"
+                    titleClassName="text-[64px]"
+                    detailClassName="text-[24px]"
+                />
+            ) : (
+                <StatusResult
+                    title="Sorry, an unexpected error has occurred"
+                    detail={error.statusText || error.message}
+                />
+            )}
+        </MainLayout>
+    );
 };
 
 export default ErrorBoundary;
