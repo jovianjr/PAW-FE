2474ece88dd54c3394c3bf4bfed807fd5fd964e1
aulianurfajriyah <aulia.nur.fajriyah@mail.ugm.ac.id>
2022-11-13 19:39:35 +0700

    hotfix(error-page): fix error code 404

--- src/views/pages/error/index.jsx
+++ src/views/pages/error/index.jsx
@@ -6,7 +6,7 @@ const ErrorBoundary = () => {
     let error = useRouteError();
     return (
         <MainLayout>
-            {error.status === 408 ? (
+            {error.status === 404 ? (
                 <StatusResult
                     type="warning"
                     title="404"
