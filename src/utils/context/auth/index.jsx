433308e590c68ba81ea4f40f65520d7f9ee9d043
Alphonsus Jovian Joy Reynaldo <55451337+jovianjr@users.noreply.github.com>
2022-11-14 21:57:51 +0700

    feat(context): add authContext (#28)

---
+++ src/utils/context/auth/index.jsx
@@ -0,0 +1,20 @@
+// Contexts.js
+import { createContext, useEffect, useReducer } from 'react';
+import { LOGIN, LOGOUT } from './type';
+import reducer from './reducer';
+
+const initialState = { user: null };
+
+export const AuthContext = createContext(initialState);
+export const AuthProvider = ({ children }) => {
+    const [state, dispatch] = useReducer(reducer, initialState);
+    const value = {
+        user: state,
+        login: val => dispatch({ type: LOGIN, payload: val }),
+        logout: val => dispatch({ type: LOGOUT, payload: val })
+    };
+
+    return (
+        <AuthContext.Provider value={value}>{children}</AuthContext.Provider>
+    );
+};
