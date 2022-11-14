4435a1e11910412bfd9040995e94f8204c71f3b2
jovian <alphonsusjovian@gmail.com>
2022-11-15 00:55:29 +0700

    hotfix(search-page): img preview

--- src/views/pages/search/data.js
+++ src/views/pages/search/data.js
@@ -1,48 +1,48 @@
 export default [
     {
-        img: 'http://localhost:5173/images/1.png',
+        img: '/images/1.png',
         title: 'John Doe',
         text: 'John Doe',
         date_created: '01 jan 2022'
     },
     {
-        img: 'http://localhost:5173/images/2.png',
+        img: '/images/2.png',
         title: 'John Doe',
         text: 'John Doe',
         date_created: '01 jan 2022'
     },
     {
-        img: 'http://localhost:5173/images/3.png',
+        img: '/images/3.png',
         title: 'John Doe',
         text: 'John Doe',
         date_created: '01 jan 2022'
     },
     {
-        img: 'http://localhost:5173/images/4.png',
+        img: '/images/4.png',
         title: 'John Doe',
         text: 'John Doe',
         date_created: '01 jan 2022'
     },
     {
-        img: 'http://localhost:5173/images/5.png',
+        img: '/images/5.png',
         title: 'John Doe',
         text: 'John Doe',
         date_created: '01 jan 2022'
     },
     {
-        img: 'http://localhost:5173/images/6.png',
+        img: '/images/6.png',
         title: 'John Doe',
         text: 'John Doe',
         date_created: '01 jan 2022'
     },
     {
-        img: 'http://localhost:5173/images/7.png',
+        img: '/images/7.png',
         title: 'John Doe',
         text: 'John Doe',
         date_created: '01 jan 2022'
     },
     {
-        img: 'http://localhost:5173/images/8.png',
+        img: '/images/8.png',
         title: 'John Doe',
         text: 'John Doe',
         date_created: '01 jan 2022'
