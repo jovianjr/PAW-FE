7b063f281f02b7b1103d5952da9e5068332b6cad
Alphonsus Jovian Joy Reynaldo <55451337+jovianjr@users.noreply.github.com>
2022-11-14 11:30:56 +0700

    Feature/navbar (#27)
    * feat(dropdown): add overlay and custom classname for item
    
    * feat(navbar): responsive

--- src/views/components/dropdown/index.jsx
+++ src/views/components/dropdown/index.jsx
@@ -1,5 +1,6 @@
 import clsx from 'clsx';
-import { Popover } from '@headlessui/react';
+import { Fragment } from 'react';
+import { Popover, Transition } from '@headlessui/react';
 
 import RenderIf from '@/views/components/render-if';
 
@@ -10,6 +11,7 @@ import RenderIf from '@/views/components/render-if';
  * @property {('left'|'right')} position
  * @property {string} [panelClassName]
  * @property {string} [itemClassName]
+ * @property {boolean} [overlay]
  * @property {React.MouseEvent} [onClick]
  */
 
@@ -22,40 +24,69 @@ const Dropdown = ({
     position = 'right',
     panelClassName = '',
     itemClassName = '',
+    overlay = false,
     onClick = () => {}
 }) => {
     return (
         <Popover className="relative h-full">
-            <Popover.Button className="h-full">{children}</Popover.Button>
+            {({ open }) => (
+                <>
+                    <Popover.Button className="h-full">
+                        {children}
+                    </Popover.Button>
+                    <Transition
+                        show={open && overlay}
+                        enter="transition-opacity duration-250"
+                        enterFrom="opacity-0"
+                        enterTo="opacity-100"
+                        leave="transition-opacity duration-500"
+                        leaveFrom="opacity-100"
+                        leaveTo="opacity-0"
+                    >
+                        <Popover.Overlay className="absolute right-0 z-10 h-screen w-screen bg-black opacity-30 transition-all" />
+                    </Transition>
 
-            <Popover.Panel
-                className={clsx(
-                    'absolute z-10 w-max',
-                    position === 'left' && 'left-0',
-                    position === 'right' && 'right-0',
-                    'flex flex-col gap-2 rounded border border-slate-300 bg-white',
-                    panelClassName
-                )}
-            >
-                {options.map((val, index) => {
-                    const Tag = false ? 'a' : 'button';
-                    return (
-                        <Tag
-                            key={index}
+                    <Transition
+                        as={Fragment}
+                        enter="transition ease-out duration-200"
+                        enterFrom="opacity-0 translate-y-1"
+                        enterTo="opacity-100 translate-y-0"
+                        leave="transition ease-in duration-150"
+                        leaveFrom="opacity-100 translate-y-0"
+                        leaveTo="opacity-0 translate-y-1"
+                    >
+                        <Popover.Panel
                             className={clsx(
-                                'flex items-center gap-2 px-4 py-2 text-start hover:bg-slate-200',
-                                itemClassName
+                                'absolute z-10 w-max',
+                                position === 'left' && 'left-0',
+                                position === 'right' && 'right-0',
+                                'flex flex-col rounded border border-slate-300 bg-white',
+                                panelClassName
                             )}
-                            onClick={() => onClick(val)}
                         >
-                            <RenderIf when={val.icon}>
-                                <val.icon className="h-5 w-5 text-inherit" />
-                            </RenderIf>
-                            {val.text}
-                        </Tag>
-                    );
-                })}
-            </Popover.Panel>
+                            {options.map((val, index) => {
+                                const Tag = false ? 'a' : 'button';
+                                return (
+                                    <Tag
+                                        key={index}
+                                        className={clsx(
+                                            'flex items-center gap-2 stroke-slate-900 px-4 py-4 text-start hover:bg-slate-200',
+                                            itemClassName,
+                                            val.className
+                                        )}
+                                        onClick={() => onClick(val)}
+                                    >
+                                        <RenderIf when={val.icon}>
+                                            <val.icon className="h-5 w-5 stroke-inherit" />
+                                        </RenderIf>
+                                        {val.text}
+                                    </Tag>
+                                );
+                            })}
+                        </Popover.Panel>
+                    </Transition>
+                </>
+            )}
         </Popover>
     );
 };
--- src/views/components/navbar/index.jsx
+++ src/views/components/navbar/index.jsx
@@ -1,7 +1,9 @@
 import { useForm } from 'react-hook-form';
 import { Link, useNavigate } from 'react-router-dom';
 import {
+    ArrowRightOnRectangleIcon,
     ArrowLeftOnRectangleIcon,
+    Bars3Icon,
     Cog6ToothIcon,
     MagnifyingGlassIcon,
     UserCircleIcon
@@ -28,7 +30,39 @@ const menuOptions = [
         icon: ArrowLeftOnRectangleIcon,
         path: '/',
         name: 'logout',
-        text: 'Log out'
+        text: 'Log out',
+        className: 'text-red-500 !stroke-red-500'
+    }
+];
+
+const menuOptionsMobile = [
+    {
+        icon: UserCircleIcon,
+        path: '/profile',
+        name: 'profile',
+        text: 'My Account'
+    },
+    {
+        icon: Cog6ToothIcon,
+        path: '/profile/settings',
+        name: 'settings',
+        text: 'Settings'
+    },
+    {
+        icon: ArrowRightOnRectangleIcon,
+        path: '/login',
+        name: 'Login',
+        text: 'Sign In',
+        className:
+            'bg-purple-500 text-white stroke-white text-center mx-4 rounded-lg flex justify-center !py-3 mt-4'
+    },
+    {
+        icon: ArrowLeftOnRectangleIcon,
+        path: '/',
+        name: 'logout',
+        text: 'Log out',
+        className:
+            'text-red-500 !stroke-red-500 text-center mx-4 rounded-lg flex justify-center !py-3 mt-4'
     }
 ];
 
@@ -53,11 +87,14 @@ const Navbar = () => {
     };
 
     return (
-        <header className="sticky top-0 flex h-16 items-center justify-between border-b border-slate-300 py-4 px-6">
+        <header className="sticky top-0 z-50 flex h-16 items-center justify-between border-b border-slate-300 py-4 px-6">
             <Link to="/">
-                <h1 className="text-lg font-black">PAW</h1>
+                <h1 className="text-base font-black lg:text-lg">PAW</h1>
             </Link>
-            <form className="w-1/2" onSubmit={handleSubmit(onSubmitSearch)}>
+            <form
+                className="hidden w-1/2 lg:block"
+                onSubmit={handleSubmit(onSubmitSearch)}
+            >
                 <TextField
                     control={control}
                     icon={MagnifyingGlassIcon}
@@ -76,13 +113,13 @@ const Navbar = () => {
                     }
                 />
             </form>
-            <div className="flex items-center gap-2">
+            <div className="hidden items-center gap-2 lg:flex">
                 <Button href="/login" variant="secondary">
                     Login
                 </Button>
                 <Button href="/signup">Sign Up</Button>
             </div>
-            <div className="flex items-center gap-4">
+            <div className="hidden items-center gap-4 lg:flex">
                 <Link
                     to="/art/new"
                     className="text-sm font-semibold hover:underline"
@@ -98,6 +135,23 @@ const Navbar = () => {
                         src="https://picsum.photos/200"
                         className="aspect-square w-8 rounded-full border border-slate-300 object-cover"
                     />
+                </Dropdown>
+            </div>
+            <div className="absolute right-0 top-0 flex h-full items-center gap-4">
+                <Link
+                    to="/art/new"
+                    className="text-sm font-semibold hover:underline"
+                >
+                    Create
+                </Link>
+                <Dropdown
+                    options={menuOptionsMobile}
+                    panelClassName="w-screen px-2 py-2"
+                    itemClassName="pr-10"
+                    onClick={val => onClickDropdown(val)}
+                    overlay
+                >
+                    <Bars3Icon className="h-6 w-10 px-2" />
                 </Dropdown>
             </div>
         </header>
