829095274432addea40bf347374eb1602d3e402c
Alphonsus Jovian Joy Reynaldo <55451337+jovianjr@users.noreply.github.com>
2022-11-15 23:38:52 +0700

    Feature/auth (#36)
    * feat(reset-password): add integration reset & forgot password
    
    * feat(auth): signup integration
    
    * feat(auth): activate account integration
    
    * hotfix(auth): retry count

--- src/utils/services/auth.jsx
+++ src/utils/services/auth.jsx
@@ -1,16 +1,60 @@
 import axios from '@/utils/helpers/axios';
 
-const login = async ({ identity, password }) => {
-    const { data } = await axios.post(
-        '/auth/login',
-        {
-            identity: identity,
-            password: password
-        },
-        { headers: null }
-    );
+const activate = async token => {
+    const { data } = await axios.get(`/auth/activate/${token}`);
 
     return data;
 };
 
-export { login };
+const forgotPassword = async email => {
+    const { data } = await axios.post('/auth/forgot-password', {
+        email: email
+    });
+
+    return data;
+};
+
+const login = async ({ identity, password }) => {
+    const { data } = await axios.post('/auth/login', {
+        identity: identity,
+        password: password
+    });
+
+    return data;
+};
+
+const resetPassword = async ({ token, password, confirmPassword }) => {
+    const { data } = await axios.patch(`/auth/reset-password/${token}`, {
+        password: password,
+        confirmPassword: confirmPassword
+    });
+
+    return data;
+};
+
+const resetPasswordCheck = async token => {
+    const { data } = await axios.get(`/auth/reset-password/${token}`);
+
+    return data;
+};
+
+const signUp = async ({ username, email, password, confirmPassword, name }) => {
+    const { data } = await axios.post('/auth/register', {
+        username: username,
+        email: email,
+        password: password,
+        confirmPassword: confirmPassword,
+        name: name
+    });
+
+    return data;
+};
+
+export {
+    activate,
+    forgotPassword,
+    login,
+    resetPassword,
+    resetPasswordCheck,
+    signUp
+};
--- src/views/pages/auth/forgot-password/index.jsx
+++ src/views/pages/auth/forgot-password/index.jsx
@@ -1,67 +1,83 @@
-import { useState } from 'react';
+import { useMutation } from '@tanstack/react-query';
 import { useForm } from 'react-hook-form';
 
+import announce from '@/views/components/announcement';
+import LoadingScreen from '@/views/components/loading';
+import RenderIf from '@/views/components/render-if';
 import StatusResult from '@/views/components/status-result';
 
 import Button from '@/views/elements/button';
 import TextField from '@/views/elements/text-field';
 
 import AuthLayout from '@/views/layouts/auth-layout';
+import MainLayout from '@/views/layouts/main-layout';
+
+import { forgotPassword } from '@/utils/services/auth';
 
 const ForgotPassword = () => {
-    const [result, setResult] = useState();
     const { control, handleSubmit } = useForm({
         defaultValues: {
             email: ''
         }
     });
 
-    if (result === 'success')
-        return (
-            <StatusResult
-                type="success"
-                title="Your password has successfully changed!"
-                button="Back to Login"
-                href="/login"
-            />
-        );
+    const forgotPasswordMutation = useMutation(data => forgotPassword(data), {
+        onSuccess: res => {
+            console.log(res);
+        },
+        onError: err => {
+            if (err.response) {
+                const { data } = err.response;
+                announce.error(data.errors);
+            } else console.log(err);
+        }
+    });
 
-    if (result === 'failed') {
-        return (
-            <StatusResult
-                type="danger"
-                title="Your password cannot be reset at this time"
-                detail="Please try again"
-                button="Back to Login"
-                href="/login"
-            />
-        );
-    }
-
-    const onSubmit = () => {
-        alert('submitted');
-        setResult('failed');
+    const onSubmit = data => {
+        forgotPasswordMutation.mutateAsync(data.email);
     };
 
     return (
-        <AuthLayout onSubmit={handleSubmit(onSubmit)}>
-            <h1 className="w-full text-center text-xl font-semibold lg:text-2xl">
-                Reset your password
-            </h1>
-            <TextField
-                control={control}
-                className="w-full"
-                name="email"
-                fieldName="email"
-                placeholder="email"
-                type="email"
-                rules={{ required: true }}
-            />
+        <>
+            <LoadingScreen when={forgotPasswordMutation.isLoading} />
+            <RenderIf when={forgotPasswordMutation.isSuccess}>
+                <MainLayout screen>
+                    <StatusResult
+                        type="success"
+                        title="Password reset email has been sent"
+                        detail="Follow the directions in the email to reset your password"
+                        button="Back to Login"
+                        href="/login"
+                        className="!h-full"
+                    />
+                </MainLayout>
+            </RenderIf>
+            <RenderIf
+                when={
+                    forgotPasswordMutation.isIdle ||
+                    forgotPasswordMutation.isError
+                }
+            >
+                <AuthLayout onSubmit={handleSubmit(onSubmit)}>
+                    <h1 className="w-full text-center text-xl font-semibold lg:text-2xl">
+                        Reset your password
+                    </h1>
+                    <TextField
+                        control={control}
+                        className="w-full"
+                        name="email"
+                        fieldName="email"
+                        placeholder="email"
+                        type="email"
+                        rules={{ required: true }}
+                    />
 
-            <Button className="w-full" type="submit">
-                Send password reset email
-            </Button>
-        </AuthLayout>
+                    <Button className="w-full" type="submit">
+                        Send password reset email
+                    </Button>
+                </AuthLayout>
+            </RenderIf>
+        </>
     );
 };
 
