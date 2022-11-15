40b3a5c6a55046b015508e1e619d21ca13a01ad7
jovian <alphonsusjovian@gmail.com>
2022-11-15 22:50:03 +0700

    feat(reset-password): add integration reset & forgot password

--- src/utils/services/auth.jsx
+++ src/utils/services/auth.jsx
@@ -1,16 +1,35 @@
 import axios from '@/utils/helpers/axios';
 
 const login = async ({ identity, password }) => {
-    const { data } = await axios.post(
-        '/auth/login',
-        {
-            identity: identity,
-            password: password
-        },
-        { headers: null }
-    );
+    const { data } = await axios.post('/auth/login', {
+        identity: identity,
+        password: password
+    });
 
     return data;
 };
 
-export { login };
+const forgotPassword = async email => {
+    const { data } = await axios.post('/auth/forgot-password', {
+        email: email
+    });
+
+    return data;
+};
+
+const resetPassword = async ({ token, password, confirmPassword }) => {
+    const { data } = await axios.patch(`/auth/reset-password/${token}`, {
+        password: password,
+        confirmPassword: confirmPassword
+    });
+
+    return data;
+};
+
+const resetPasswordCheck = async token => {
+    const { data } = await axios.get(`/auth/reset-password/${token}`);
+
+    return data;
+};
+
+export { login, forgotPassword, resetPassword, resetPasswordCheck };
--- src/views/pages/auth/reset-password/index.jsx
+++ src/views/pages/auth/reset-password/index.jsx
@@ -1,79 +1,140 @@
-import { useState } from 'react';
+import { useQuery, useMutation } from '@tanstack/react-query';
 import { useForm } from 'react-hook-form';
+import { useSearchParams } from 'react-router-dom';
 
+import announce from '@/views/components/announcement';
+import LoadingScreen from '@/views/components/loading';
 import StatusResult from '@/views/components/status-result';
+import RenderIf from '@/views/components/render-if';
 
 import Button from '@/views/elements/button';
 import TextField from '@/views/elements/text-field';
 
 import AuthLayout from '@/views/layouts/auth-layout';
+import MainLayout from '@/views/layouts/main-layout';
+
+import { resetPassword, resetPasswordCheck } from '@/utils/services/auth';
 
 const ResetPassword = () => {
-    const [result, setResult] = useState();
-
+    let [searchParams] = useSearchParams();
     const { control, handleSubmit } = useForm({
         defaultValues: {
             password: '',
-            confirm_password: ''
+            confirmPassword: ''
         }
     });
 
-    if (result === 'success')
-        return (
-            <StatusResult
-                type="success"
-                title="Your password has successfully changed!"
-                button="Back to Login"
-                href="/login"
-            />
-        );
+    const { data, isLoading, isFetching, isError } = useQuery(
+        ['reset-password-check'],
+        () => resetPasswordCheck(searchParams.get('token')),
+        {
+            refetchOnWindowFocus: false,
+            refetchInterval: false,
+            onSuccess: res => {
+                console.log('session', res.data);
+            },
+            onError: err => {},
+            retry: (failureCount, error) => {
+                if (error?.response?.status === 498) return false;
+                else return true;
+            }
+        }
+    );
 
-    if (result === 'failed') {
-        return (
-            <StatusResult
-                type="danger"
-                title="Your password cannot be reset at this time"
-                detail="Please try again"
-                button="Back to Login"
-                href="/login"
-            />
-        );
-    }
+    const resetPasswordMutation = useMutation(data => resetPassword(data), {
+        onSuccess: res => {
+            console.log(res);
+        },
+        onError: err => {
+            if (err.response) {
+                const { data } = err.response;
+                announce.error(data.errors);
+            } else console.log(err);
+        }
+    });
 
-    const onSubmit = () => {
-        alert('submitted');
-        setResult('failed');
+    const onSubmit = data => {
+        resetPasswordMutation.mutateAsync({
+            token: searchParams.get('token'),
+            password: data.password,
+            confirmPassword: data.confirmPassword
+        });
     };
-    return (
-        <AuthLayout onSubmit={handleSubmit(onSubmit)}>
-            <h1 className="w-full text-center text-xl font-semibold lg:text-2xl">
-                Change password for @user
-            </h1>
-            <div className="flex w-full flex-col gap-4">
-                <TextField
-                    control={control}
-                    className="w-full"
-                    name="password"
-                    fieldName="Password"
-                    placeholder="Password"
-                    type="password"
-                    rules={{ required: true }}
-                />
-                <TextField
-                    control={control}
-                    className="w-full"
-                    name="confirm_password"
-                    fieldName="Confirm Password"
-                    placeholder="Confirm Password"
-                    type="password"
-                    rules={{ required: true }}
-                />
-            </div>
 
-            <Button className="w-full" type="submit">
-                Change password
-            </Button>
-        </AuthLayout>
+    return (
+        <>
+            <LoadingScreen
+                when={
+                    isLoading || isFetching || resetPasswordMutation.isLoading
+                }
+            />
+            <RenderIf when={resetPasswordMutation.isSuccess}>
+                <MainLayout screen>
+                    <StatusResult
+                        type="success"
+                        title="Your password has successfully changed!"
+                        button="Back to Login"
+                        href="/login"
+                        className="!h-full"
+                    />
+                </MainLayout>
+            </RenderIf>
+            <RenderIf when={isError}>
+                <MainLayout screen>
+                    <StatusResult
+                        type="danger"
+                        title="Invalid / Expired Token"
+                        button="Back to Login"
+                        href="/login"
+                        className="!h-full"
+                    />
+                </MainLayout>
+            </RenderIf>
+            <RenderIf when={resetPasswordMutation.isError}>
+                <MainLayout screen>
+                    <StatusResult
+                        type="danger"
+                        title="Your password cannot be reset at this time"
+                        detail={
+                            resetPasswordMutation?.error?.response?.data
+                                ?.errors ?? 'Please try again'
+                        }
+                        button="Back to Login"
+                        href="/login"
+                        className="!h-full"
+                    />
+                </MainLayout>
+            </RenderIf>
+            <AuthLayout onSubmit={handleSubmit(onSubmit)}>
+                <h1 className="w-full text-center text-xl font-semibold lg:text-2xl">
+                    Change password for @{data?.data?.username}
+                </h1>
+                <div className="flex w-full flex-col gap-4">
+                    <TextField
+                        control={control}
+                        className="w-full"
+                        name="password"
+                        fieldName="Password"
+                        placeholder="Password"
+                        type="password"
+                        rules={{ required: true }}
+                    />
+                    <TextField
+                        control={control}
+                        className="w-full"
+                        name="confirmPassword"
+                        fieldName="Confirm Password"
+                        placeholder="Confirm Password"
+                        type="password"
+                        rules={{ required: true }}
+                    />
+                </div>
+
+                <Button className="w-full" type="submit">
+                    Change password
+                </Button>
+            </AuthLayout>
+        </>
     );
 };
 
